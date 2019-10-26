384e19f282459ee5164de5590659ef7ceebc6bfc
Ivan Milic <milic.tg@gmail.com>
2019-10-26 19:46:03 +0200

    Install router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import NavBar from "./components/shared/NavBar";
+import IndexPage from "./components/pages/IndexPage";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Radio stranica</h1>
+      <IndexPage />
     </div>
   );
 }
---
+++ src/components/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class IndexPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pocetna</h1>
+      </div>
+    );
+  }
+}
