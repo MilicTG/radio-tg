2ee79bf496eafab1b76f2c7e1318f187211c9eec
Ivan Milic <milic.tg@gmail.com>
2019-10-26 21:00:00 +0200

    shitting around with styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import NavBar from "./components/shared/NavBar";
 import IndexPage from "./components/pages/IndexPage";
+import ArchivePage from "./components/pages/ArchivePage";
+import MarketingPage from "./components/pages/MarketingPage";
+import ContactPage from "./components/pages/ContactPage";
+import FourZeroFourPage from "./components/pages/404Page";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 export default class NavBar extends Component {
   render() {
-    return <h1>Navigacija</h1>;
+    return (
+      <NavContainer>
+        <h1>Navigacija</h1>
+      </NavContainer>
+    );
   }
 }
+
+const NavContainer = styled.nav`
+  display: block;
+  width: 100%;
+  height: 80px;
+  background-color: #f2f2f2;
+`;
