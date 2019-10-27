ae1162eb156c51913e87584c3d41518f0e758d0a
Ivan Milic <milic.tg@gmail.com>
2019-10-27 09:45:39 +0100

    fixing router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React from "react";
-import NavBar from "./components/shared/NavBar";
+import React, { Component } from "react";
 import IndexPage from "./components/pages/IndexPage";
 import ArchivePage from "./components/pages/ArchivePage";
 import MarketingPage from "./components/pages/MarketingPage";
 import ContactPage from "./components/pages/ContactPage";
 import FourZeroFourPage from "./components/pages/404Page";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "./components/shared/theme";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <h1>Radio stranica</h1>
-      <IndexPage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <NavContainer>
+          <h1>Logo</h1>
+
+          <ul>
+            <li>
+              <Link to="/">Pocetna</Link>
+            </li>
+            <li>
+              <Link to="/arhiva">Arhiva</Link>
+            </li>
+            <li>
+              <Link to="/marketing">Marketing</Link>
+            </li>
+            <li>
+              <Link to="/kontakt">Kontakt</Link>
+            </li>
+          </ul>
+        </NavContainer>
+
+        <Switch>
+          <Route path="/">
+            <IndexPage />
+          </Route>
+          <Route path="/arhiva">
+            <ArchivePage />
+          </Route>
+          <Route path="/marketing">
+            <MarketingPage />
+          </Route>
+          <Route path="/kontakt">
+            <ContactPage />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
+
+const NavContainer = styled.nav`
+  width: 100%;
+  height: 80px;
+  background: ${theme.colorLightBck};
+`;
 
 export default App;
---
+++ src/components/shared/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  //main colors
+  colorPrimary: "#0A4E8F",
+  colorSecondary: "#0A79A9",
+  colorLightBck: "#F5F9F3",
+  colorDarkerBck: "#F1EBD2",
+  colorFont: "#303030",
+  //font size
+  h1: "6.7rem",
+  h2: "5rem",
+  h3: "3.7rem",
+  h4: "2.8rem",
+  h5: "2.1rem",
+  p: "1.2rem"
+};
