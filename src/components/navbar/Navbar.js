b8468ac2f441cba00704a3542039d6caf2f9e90c
Ivan Milic <milic.tg@gmail.com>
2020-02-19 17:22:08 +0100

    Keep trying man

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,6 +9,20 @@ const Navbar = props => (
    <NavbarWrapper>
       <NavbarContainer>
          <Logo />
+         <Spacer />
+         <NavigationItems>
+            <ul>
+               <li>
+                  <Link to='/program'>Program</Link>
+               </li>
+               <li>
+                  <Link to='/marketing'>Marketing</Link>
+               </li>
+               <li>
+                  <Link to='/kontakt'>Kontakt</Link>
+               </li>
+            </ul>
+         </NavigationItems>
       </NavbarContainer>
    </NavbarWrapper>
 );
@@ -25,15 +39,18 @@ const NavbarContainer = styled.div`
    display: flex;
    margin: auto;
    padding: 0 2rem;
-   justify-content: space-between;
    align-content: center;
-
+   height: 100%;
    @media ${device.deviceDesktop} {
       max-width: 1100px;
    }
    @media ${device.deviceDesktopHD} {
       max-width: 1440px;
    }
+`;
+
+const Spacer = styled.div`
+   flex: 1;
 `;
 
 const NavigationItems = styled.div``;
